25f8ec69255085a70e599984e2a2f7109a34522f
Krish Agarwal <krish.agarwal@kalvium.community>
2025-03-18 00:31:05 +0530

    milestone 28

--- frontend/src/components/auth/CartProduct.jsx
+++ frontend/src/components/auth/CartProduct.jsx
@@ -33,7 +33,7 @@ export default function CartProduct({ _id, name, images, quantity, price }) {
 				'Content-Type': 'application/json',
 			},
 			body: JSON.stringify({
-				email: 'krishagarwal52139@gamil.com',
+				email: 'krishagarwal52139@gmail.com',
 				productId: _id,
 				quantity,
 			}),
